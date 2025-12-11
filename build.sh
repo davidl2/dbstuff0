46378621a6814562ebdb6bed5c9b533e584d33f7
David Lee Lambert <davidl@lmert.com>
2025-12-11 16:32:58 -0500

    (use a temp file for some queries; and include the commit hash in log)

--- build.sh
+++ build.sh
@@ -9,10 +9,16 @@ fi
 
 mysql $db -e "INSERT INTO log (message) VALUES ('Start. Building on: `uname`')"
 
+mkdir -p build
+echo '' >build/build_info.sql
+
 for var in BUILD_NUMBER NODE_NAME JOB_NAME WORKSPACE BUILD_URL
 do
-    mysql $db -e "INSERT INTO log (message) VALUES ('  $var=${!var}')"
+    echo "INSERT INTO log (message) VALUES ('  $var=${!var}'); " >>build/build_info.sql
 done
+echo "INSERT INTO log (message) VALUES ('  commit=`git rev-parse HEAD`'); " >>build/build_info.sql
+
+mysql $db <build/build_info.sql
 
 sleep 5
 
