e101640a56870ea88bcdafe59b2e8d05ca4bd501
David Lee Lambert <davidl@lmert.com>
2025-12-11 16:16:23 -0500

    (different flow for local dev, not CI)

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+if [ "$CI" = true ]
+then
+    db=test
+else
+    db=$USER
+fi
 
-mysql test -e "INSERT INTO log (message) VALUES ('Start. Building on: `uname`')"
+mysql $db -e "INSERT INTO log (message) VALUES ('Start. Building on: `uname`')"
 
 for var in BUILD_NUMBER NODE_NAME JOB_NAME WORKSPACE BUILD_URL
 do
-    mysql test -e "INSERT INTO log (message) VALUES ('  $var=${!var}')"
+    mysql $db -e "INSERT INTO log (message) VALUES ('  $var=${!var}')"
 done
 
 sleep 5
 
-mysql test -e "INSERT INTO log (message) VALUES ('End.')"
+mysql $db -e "INSERT INTO log (message) VALUES ('End.')"
 
