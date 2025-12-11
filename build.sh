15bcd615d9b5f43ded93c19ad90866cbf27c1938
David Lee Lambert <davidl@lmert.com>
2025-12-11 15:37:52 -0500

    (log nore info)

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 
 mysql test -e "INSERT INTO log (message) VALUES ('Start. Building on: `uname`')"
 
+for var in BUILD_NUMBER NODE_NAME JOB_NAME WORKSPACE BUILD_URL
+do
+    mysql test -e "INSERT INTO log (message) VALUES ('  $var=${!var}')"
+done
+
 sleep 5
 
 mysql test -e "INSERT INTO log (message) VALUES ('End.')"
